b4ce987e93f0cac854f0c0f4beae9cbecd132307
drew21 <drewgriz@gmail.com>
2020-02-04 14:43:42 +0200

    remove some duplicated dir

--- ENGineering-me-SOFTly/api_client/src/main/groovy/gr/ntua/ece/softeng18b/client/model/PriceInfoList.groovy
+++
@@ -1,7 +0,0 @@
-package gr.ntua.ece.softeng18b.client.model
-
-class PriceInfoList extends Paging {
-
-    List<PriceInfo> prices
-
-}
--- ENGineering-me-SOFTly/api_client/src/test/groovy/gr/ntua/ece/softeng18b/client/TestData.groovy
+++
@@ -1,7 +0,0 @@
-package gr.ntua.ece.softeng18b.client
-
-class TestData {
-
-
-
-}
